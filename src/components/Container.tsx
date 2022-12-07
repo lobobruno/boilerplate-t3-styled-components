7cd1455eec5b087c9f94a39a5d6a59f85bb6d78e
lobobruno <brunolobo.df@gmail.com>
2022-12-07 16:12:55 -0300

    :zap: add styled components

---
+++ src/components/Container.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Container = styled.div.attrs({
+  className:
+    "container flex flex-col items-center justify-center gap-12 px-4 py-16",
+})``;
+
+export default Container;
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const MainPage = styled.div.attrs({
+  className:
+    "flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]",
+})``;
+
+export default MainPage;
